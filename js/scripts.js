c39acd3fa3da79938fe76b18a8cb396cafbc0f59
Jason Bethel <jlbethel@gmail.com>
2015-09-11 09:10:21 -0700

    write findReplace function to pass first spec

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+var findReplace = function(string, subString) {
+  var searchString = string;
+  var replaceWord = subString;
+  var output = '';
+
+  if(searchString.indexOf(subString) != -1) {
+    output += replaceWord + ' does occur in your string';
+  }
+  return output;
+}
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,5 @@
 describe('findReplace', function() {
-  it("will take a users string and search for a chosen word in that string. String is one letter long", funciton () {
+  it("will take a users string and search for a chosen word in that string. String is one letter long", function () {
     expect(findReplace('a', 'a')).to.equal('a does occur in your string');
   })
 });
