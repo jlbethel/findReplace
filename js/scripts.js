2d8791f25e64bbd940a16adfc23fed4557545a90
Jason Bethel <jlbethel@gmail.com>
2015-09-11 09:44:01 -0700

    write spec that looks for multiple instances of word to be replaced

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,10 @@ var findReplace = function(string, replaceWord, newWord) {
   var newString = '';
 
   if(string.indexOf(replaceWord) != -1) {
-    output += replaceWord+ ' does occur in your string';
-    // newString = string.replace(replaceWord, newWord);
-  } else{ output += replaceWord + ' does not occur in your string'}
-  return output;
+    // output += replaceWord+ ' does occur in your string';
+    newString = string.replace(replaceWord, newWord);
+  } else{ newString += replaceWord + ' does not occur in your string'}
+  return newString;
+  // return newString;
 
 }
--- spec/specs.js
+++ spec/specs.js
@@ -1,18 +1,22 @@
 describe('findReplace', function() {
-  //questions for Dianne. Are these first three tests silly from a BDD stand point? I have to modify them to match my changing funtion
-  it("will take a users string and search for a chosen word in that string. String is one letter long", function () {
-    expect(findReplace('a', 'a')).to.equal('a does occur in your string');
-  });
-
-  it("will take a users string and search for a chosen word in that string. String is two words long", function () {
-    expect(findReplace('the dog', 'dog')).to.equal('dog does occur in your string');
-  });
+  //questions for Dianne. Are these first two tests silly from a BDD stand point? I have to comment them out as I add functionality. I decided to leave them commented out rather than remove them so I remember to ask you.
+  // it("will take a users string and search for a chosen word in that string. String is one letter long", function () {
+  //   expect(findReplace('a', 'a')).to.equal('a does occur in your string');
+  // });
+  //
+  // it("will take a users string and search for a chosen word in that string. String is two words long", function () {
+  //   expect(findReplace('the dog', 'dog')).to.equal('dog does occur in your string');
+  // });
 
   it("will look for a word in the user's string, find that it does not occur, and return appropriate response", function () {
     expect(findReplace('the dog', 'cat')).to.equal('cat does not occur in your string');
   });
 
-  it("will lood for a word in user's string, if present, will replace it with user's chosen word", function() {
+  it("will look for a word in user's string, if present, will replace it with user's chosen word", function() {
     expect(findReplace('the dog', 'dog', 'cat')).to.equal('the cat');
+  });
+
+  it("will look for a word in a user's string, will find multiple instances of it, ane replace them all with the user's chosen word", function () {
+    expect(findReplace('the dog met the other dog and wagged her tail', 'dog', 'cat')).to.equal('the cat met the other cat and wagged her tail');
   });
 });
