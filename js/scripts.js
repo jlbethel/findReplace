ee691818ada9385647d49322bcbaaf36da9c18d5
Jason Bethel <jlbethel@gmail.com>
2015-09-11 09:35:35 -0700

    change variable names in findReplace to be more clear

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,12 @@
-var findReplace = function(string, subString) {
-  var searchString = string;
-  var replaceWord = subString;
-  var output = '';
+var findReplace = function(string, replaceWord, newWord) {
 
-  if(searchString.indexOf(subString) != -1) {
-    output += replaceWord + ' does occur in your string';
+  var output = '';
+  var newString = '';
+
+  if(string.indexOf(replaceWord) != -1) {
+    output += replaceWord+ ' does occur in your string';
+    // newString = string.replace(replaceWord, newWord);
   } else{ output += replaceWord + ' does not occur in your string'}
   return output;
+
 }
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,5 @@
 describe('findReplace', function() {
-  //questions for Dianne. Are these first three tests silly from a BDD stand point?
+  //questions for Dianne. Are these first three tests silly from a BDD stand point? I have to modify them to match my changing funtion
   it("will take a users string and search for a chosen word in that string. String is one letter long", function () {
     expect(findReplace('a', 'a')).to.equal('a does occur in your string');
   });
@@ -10,5 +10,9 @@ describe('findReplace', function() {
 
   it("will look for a word in the user's string, find that it does not occur, and return appropriate response", function () {
     expect(findReplace('the dog', 'cat')).to.equal('cat does not occur in your string');
+  });
+
+  it("will lood for a word in user's string, if present, will replace it with user's chosen word", function() {
+    expect(findReplace('the dog', 'dog', 'cat')).to.equal('the cat');
   });
 });
