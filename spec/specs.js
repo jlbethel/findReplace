1b49d281ccaae8566702776fe1908755a5e5afe1
Jason Bethel <jlbethel@gmail.com>
2015-09-11 09:12:52 -0700

    write second spec(passing)

--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,9 @@
 describe('findReplace', function() {
   it("will take a users string and search for a chosen word in that string. String is one letter long", function () {
     expect(findReplace('a', 'a')).to.equal('a does occur in your string');
-  })
+  });
+
+  it("will take a users string and search for a chosen word in that string. String is two words long", function () {
+    expect(findReplace('the dog', 'dog')).to.equal('dog does occur in your string');
+  });
 });
